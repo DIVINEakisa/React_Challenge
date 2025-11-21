dacaba24e74be92ac8d673e3795bee7c23b35ff6
DIVINEakisa <akisadivine11@gmail.com>
2025-11-21 22:26:10 +0200

    Add New Component Food

--- react-chall/src/App.jsx
+++ react-chall/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Food from "./components/Food";
 function App() {
   return (
     <>
       <Header />
       <Footer />
+      <Food />
     </>
   );
 }
--- react-chall/src/components/Footer.jsx
+++ react-chall/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer>
-      <p>&copy; My Website</p>
+      <p>&copy;{new Date().getFullYear()} My Website</p>
     </footer>
   );
 }
