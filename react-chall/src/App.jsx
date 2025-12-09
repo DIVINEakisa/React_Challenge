cf186c4400564e83447b80a434b999d309203d14
DIVINEakisa <akisadivine11@gmail.com>
2025-12-09 16:52:04 +0200

    Added a progress bar component and integrated it into the main app.

--- react-chall/src/App.jsx
+++ react-chall/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Food from "./components/Food";
 import Button from "./components/Button";
+import Button from "./components/ProgressBar";
 function App() {
   return (
     <>
